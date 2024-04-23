1956e23191eb98278be4188d7d2709dfcd63d3e6
Ahmed Esmail <107895995+AhmedEsmail91@users.noreply.github.com>
2024-04-23 06:15:31 +0200

    Creating Repo

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+# make route for the homelanding page called "/"
+Route::get('/', function () {
+    return response()->json(['message' => 'Hello World!'], 200);
+});
+
